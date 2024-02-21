27f48fa984d0b7a544fff8082aa82b8e88f2364b
Santosh Narvekar <120112762+santyy123@users.noreply.github.com>
2024-02-22 02:18:45 +0530

    Update ReplyComponent.jsx

--- frontend/src/components/ReplyComponent.jsx
+++ frontend/src/components/ReplyComponent.jsx
@@ -29,9 +29,9 @@ const ReplyComponent = ({username,profilePhoto,userCommentText,userId,postId,rep
   
   const handleDelete =async (e)=>{
     e.preventDefault();
-    const deleteEl =  document.getElementById(`deletePostComment${curIndex+1}`);
     const res = window.confirm('do you want to delete this comment?');
     if(res){
+    const deleteEl =  document.getElementById(`deletePostComment${curIndex+1}`);
       deleteEl.disabled = true; 
       const response = await  dispatch(deletePostComment(replyId));
       if(response.type=='/deletePostComment/fulfilled'){
